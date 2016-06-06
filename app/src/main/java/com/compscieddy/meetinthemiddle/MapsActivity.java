d66c74f7617639ea48d54823e332dd905b681f09
Eddie Lee <eddieberklee@gmail.com>
2016-06-06 00:30:24 -0700

    Apparently either FINE *or* COARSE location should be asked for in AndroidManifest, not both

--- app/src/main/java/com/compscieddy/meetinthemiddle/MapsActivity.java
+++ app/src/main/java/com/compscieddy/meetinthemiddle/MapsActivity.java
@@ -86,6 +86,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
   @Override
   public void onLocationChanged(Location location) {
+    lawg.e("onLocationChanged");
     double latitude = location.getLatitude();
     double longitude = location.getLongitude();
     mLastKnownCoord.set(latitude, longitude);
@@ -123,9 +124,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
 
-    // Add a marker in Sydney and move the camera
-    LatLng sydney = new LatLng(-34, 151);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
   }
 }
