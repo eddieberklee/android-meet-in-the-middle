f892b9e25315f6ac45ea2e404e706698162e6126
DarrenPoh <dpoh89@gmail.com>
2016-06-07 16:33:53 +1200

    Made Map marker into circular images

--- app/src/main/java/com/compscieddy/meetinthemiddle/MapsActivity.java
+++ app/src/main/java/com/compscieddy/meetinthemiddle/MapsActivity.java
@@ -3,6 +3,13 @@ package com.compscieddy.meetinthemiddle;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -193,7 +200,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng latLng = new LatLng(latitude, longitude);
         mLastKnownCoord.set(latitude, longitude);
         if (mCurrentMarker != null) mCurrentMarker.remove();
-        mCurrentMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_darren)));
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                R.drawable.ic_darren);
+
+        Bitmap icon1 = getCroppedBitmap(icon);
+
+        mCurrentMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.fromBitmap(icon1)));
+        //mCurrentMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_darren)));
       }
     } catch (SecurityException se) {
       lawg.e("se: " + se);
@@ -209,4 +223,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
   }
+
+  public Bitmap getCroppedBitmap(Bitmap bitmap) {
+    Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+            bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(output);
+
+    final int color = 0xff424242;
+    final Paint paint = new Paint();
+    final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+    paint.setAntiAlias(true);
+    canvas.drawARGB(0, 0, 0, 0);
+    paint.setColor(color);
+    // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+    canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+            bitmap.getWidth() / 2, paint);
+    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+    canvas.drawBitmap(bitmap, rect, rect, paint);
+    //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+    //return _bmp;
+    return output;
+  }
+  
 }
