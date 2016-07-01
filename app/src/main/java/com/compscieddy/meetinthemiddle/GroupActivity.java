2f2bf519d9bf3b47d9f825e2ab5ed84501c4b8db
Eddie Lee <eddieberklee@gmail.com>
2016-06-30 22:31:59 -0700

    Activity recognition, google client null fix

--- app/src/main/java/com/compscieddy/meetinthemiddle/GroupActivity.java
+++ app/src/main/java/com/compscieddy/meetinthemiddle/GroupActivity.java
@@ -1,7 +1,6 @@
 package com.compscieddy.meetinthemiddle;
 
 import android.Manifest;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -91,7 +90,6 @@ public class GroupActivity extends FragmentActivity implements OnMapReadyCallbac
   private boolean mIsLocationPermissionEnabled = false;
 
   private final int ANIMATE_CAMERA_REPEAT = 2000;
-  private final int ACTIVITY_REFRESH_MILLIS = 3000;
 
   public static final String ARG_GROUP_KEY = "group_id_key";
   private String mGroupKey;
@@ -217,9 +215,6 @@ public class GroupActivity extends FragmentActivity implements OnMapReadyCallbac
           .addApi(LocationServices.API)
           .addApi(ActivityRecognition.API)
           .build();
-      Intent intent = new Intent(this, ActivityRecognitionService.class);
-      PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-      ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient, ACTIVITY_REFRESH_MILLIS, pendingIntent);
     }
 
     mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
